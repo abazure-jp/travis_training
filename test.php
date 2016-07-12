628d5664e899e3fe5a187b568902437fc0a8dd62
say <say.0213@gmail.com>
2016-07-12 23:29:14 +0900

    💩

--- test.php
+++ test.php
@@ -6,3 +6,11 @@ echo "ᕙ( ˘ω˘ )◜⁾💣⁽⁽◝( ˘ω˘ )ᕗ";
 echo $tesstEadf;
     echo "aaaa";
   echo "safdsaf";
+
+$array = Array("unpoko" => "💩");
+  var_dump($array);
+
+foreach($array as $shit){
+ var_dump($shit);
+
+}
