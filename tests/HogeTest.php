c27872d335475dd6ca387cb426e7c2b264376f26
abazure_jp <say.0213@gmail.com>
2016-09-21 17:43:07 +0900

    ᕙ( ˘ω˘ )◜⁾💣⁽⁽◝( ˘ω˘ )ᕗ

--- tests/HogeTest.php
+++ tests/HogeTest.php
@@ -2,13 +2,12 @@
 class HogeTest extends PHPUnit_Framework_TestCase
 {
   public function testMakeUnpoko(){
+    require 'src/Hoge.php';
+
     $a = new Hoge(3);
     $b = $a->addUnpoko();
     $this->assertEquals("3💩",$b);
   }
 
 }
-
-
-
 ?>
